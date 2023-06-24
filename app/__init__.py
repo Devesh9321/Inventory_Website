d2dfa8cdec6d138ae6aa71486356bd97bcaf355d
DeveshPatil921 <107165407+DeveshPatil921@users.noreply.github.com>
2023-06-24 13:20:42 +0000

    i will added some html file for the website including index page and define the other pages for it also. I will only use the bootstrap now, futher i will modify it.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,63 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+data = [{
+  'id': 3883849384989482,
+  'name': 'Devesh Patil'
+}, {
+  'id': 3883849384989482,
+  'name': 'Prasanjeet Kalpande'
+}, {
+  'id': 3883844384989482,
+  'name': 'Shreyash'
+}]
+
+feedbacks = [{
+  'id':
+  14447144574444543542,
+  'name':
+  'Rohan Gosavi',
+  'work':
+  'Strategic Advisor at Company LLC',
+  'message':
+  'Mi semper risus ultricies orci pulvinar in at enim orci. Quis facilisis nunc pellentesque in ullamcorper sit. Lorem blandit arcu sapien, senectus libero, amet dapibus cursus quam. Eget pellentesque eu purus volutpat adipiscing malesuada. Purus nisi, tortor vel lacus'
+}, {
+  'id':
+  14447144574444543542,
+  'name':
+  'Samiksha Ghulany',
+  'work':
+  'Head of Marketing at Lorem Ltd.',
+  'message':
+  'Vestibulum nunc lectus auctor quis. Natoque lectustortor lacus, eu. Nunc feugiat nisl maecenas nulla hac morbi. Vitae,donec facilisis sed nunc netus. Venenatis posuere faucibus enim est. Veldignissim morbi blandit morbi tellus. Arcu ullamcorper quis enim.'
+}, {
+  'id':
+  14447144574444543542,
+  'name':
+  'Gaurav Pawar',
+  'work':
+  'Project Manager at Ipsum Ltd',
+  'message':
+  'Ac at sed sit senectus massa. Massa ante amet ultrices magna porta tempor. Aliquet diam in et magna ultricies mi at. Lectus enim, vel enim egestas nam pellentesque et leo. Elit mi faucibus laoreet aliquam pellentesque sed aliquet integer massa. Orci leo tortor ornare.'
+}]
+
 
 @app.route('/')
-def hello():
-  return 'Hello, World!\n well come to website'
+def index():
+  return render_template('index.html', jobs=data, feed=feedbacks)
+
+
+@app.route('/login')
+def Clogin():
+  return render_template('login/login.html')
+
+
+@app.route('/Departmentlogin')
+def Elogin():
+  return render_template('login/loginEmp.html')
+
+
+@app.route('/UserRegistration')
+def userReg():
+  return render_template('login/Registration.html')
---
+++ app/static/js/script.js
@@ -0,0 +1,127 @@
+$(window).on("load", function () {
+    if ($(".preloader").length) {
+      $(".preloader").fadeOut();
+    }
+    thmSwiperInit();
+    thmOwlInit();
+    projectMasonaryLayout();
+    priceFilter()
+
+
+    if ($(".curved-circle--item").length) {
+      $(".curved-circle--item").circleType();
+    }
+
+    //Jquery Spinner / Quantity Spinner
+    if ($('.quantity-spinner').length) {
+      $("input.quantity-spinner").TouchSpin({
+        verticalbuttons: true
+      });
+    }
+
+
+
+    if ($(".post-filter").length) {
+      var postFilterList = $(".post-filter li");
+      // for first init
+      $(".filter-layout").isotope({
+        filter: ".filter-item",
+        animationOptions: {
+          duration: 500,
+          easing: "linear",
+          queue: false
+        }
+      });
+      // on click filter links
+      postFilterList.on("click", function () {
+        var Self = $(this);
+        var selector = Self.attr("data-filter");
+        postFilterList.removeClass("active");
+        Self.addClass("active");
+
+        $(".filter-layout").isotope({
+          filter: selector,
+          animationOptions: {
+            duration: 500,
+            easing: "linear",
+            queue: false
+          }
+        });
+        return false;
+      });
+    }
+
+    if ($(".post-filter.has-dynamic-filter-counter").length) {
+      // var allItem = $('.single-filter-item').length;
+
+      var activeFilterItem = $(".post-filter.has-dynamic-filter-counter").find(
+        "li"
+      );
+
+      activeFilterItem.each(function () {
+        var filterElement = $(this).data("filter");
+        var count = $(".filter-layout").find(filterElement).length;
+        $(this).append("<sup>[" + count + "]</sup>");
+      });
+    }
+
+    //Testimonials Two
+    if ($(".listing-details__gallery .bxslider").length) {
+      $(".listing-details__gallery .bxslider").bxSlider({
+        nextSelector: ".listing-details__gallery #slider-next",
+        prevSelector: ".listing-details__gallery #slider-prev",
+        nextText: '<i class="icon-right-arrow1"></i>',
+        prevText: '<i class="icon-right-arrow1 icon-prev"></i>',
+        mode: "horizontal",
+        auto: "true",
+        speed: "1000",
+        pagerCustom: ".listing-details__gallery .slider-pager .listing-details__thumb-box"
+      });
+    }
+
+
+
+    if ($("#testimonials-two__thumb").length) {
+      let testimonialsThumb = new Swiper("#testimonials-two__thumb", {
+        slidesPerView: 3,
+        spaceBetween: 0,
+        speed: 1400,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        loop: true,
+        autoplay: {
+          delay: 5000
+        }
+      });
+
+      let testimonialsCarousel = new Swiper("#testimonials-two__carousel", {
+        observer: true,
+        observeParents: true,
+        speed: 1400,
+        mousewheel: false,
+        slidesPerView: 1,
+        autoplay: {
+          delay: 5000
+        },
+        thumbs: {
+          swiper: testimonialsThumb
+        },
+        pagination: {
+          el: '#testimonials-one__carousel-pagination',
+          type: 'bullets',
+          clickable: true
+        },
+      });
+    }
+
+
+
+
+
+
+
+
+
+
+
+  });
